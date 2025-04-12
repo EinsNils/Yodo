316863aefaa247ebbe7a03bca9d28997992f48aa
Nils Zentgraf <nils.zentgraf@aol.com>
2025-04-12 16:32:38 +0200

    feat: add default image and do some nice stuff

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { LoginComponent } from './features/auth/login/login.component';
-import { RegisterComponent } from './features/auth/register/register.component';
 
 export const routes: Routes = [
   {
@@ -11,10 +10,6 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
-  },
-  {
-    path: 'register',
-    component: RegisterComponent
   },
   // Add more routes here as we develop other features
   {
