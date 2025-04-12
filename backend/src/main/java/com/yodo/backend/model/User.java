7bd64c959a05ccea334ecde4c7881119e90cb3cc
Nils Zentgraf <nils.zentgraf@aol.com>
2025-04-12 22:15:03 +0200

    fix: not return user uuid and password

--- backend/src/main/java/com/yodo/backend/model/User.java
+++ backend/src/main/java/com/yodo/backend/model/User.java
@@ -1,5 +1,6 @@
 package com.yodo.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import java.util.UUID;
 import lombok.Data;
@@ -11,6 +12,7 @@ public class User {
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
+  @JsonIgnore
   private UUID uuid;
 
   private String firstName;
@@ -20,5 +22,5 @@ public class User {
   @Column(unique = true)
   private String email;
 
-  private String password;
+  @JsonIgnore private String password;
 }
